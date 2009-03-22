4bf8fbbace93badfed5a669ad8c2892985be9741
Jacob Kaplan-Moss <jacob@jacobian.org>
2009-03-22 16:16:24 -0500

    Stubbed out edit pages.

---
+++ django/views/generic2/edit.py
@@ -0,0 +1,13 @@
+from django.views.generic2 import DetailView
+
+class FormView(DetailView):
+    pass
+    
+class CreateView(FormView):
+    pass
+    
+class UpdateView(FormView):
+    pass
+    
+class DeleteView(DetailView):
+    pass
